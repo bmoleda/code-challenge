9e75daabc2c058eecb3a2a1b01c460e01a81e508
bmoleda <ba.moleda+github@gmail.com>
2025-01-07 15:44:47 +1100

    Allow adding new clinic in doctors edit and create forms

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -21,16 +21,11 @@ class DoctorController extends Controller
         $clinics = Clinic::all();
 
         return view('doctors.create', compact('clinics'));
-
     }
 
     public function store(StoreDoctorRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => '',
-            'specialty' => '',
-            'clinic_id' => 'exists:clinics,id',
-        ]);
+        $validatedData = $this->validateInput($request);
 
         Doctor::create($validatedData);
 
@@ -44,12 +39,42 @@ class DoctorController extends Controller
 
     public function edit(Doctor $doctor)
     {
-        return view('doctors.edit', compact('doctor'));
+        $clinics = Clinic::all();
+
+        return view('doctors.edit', compact('doctor'), compact('clinics'));
     }
 
     public function update(UpdateDoctorRequest $request, Doctor $doctor)
     {
-        $doctor->update($request->validated());
+        $validatedData = $this->validateInput($request);
+
+        $doctor->update($validatedData);
         return redirect()->route('doctors.index')->with('success', 'Doctor updated successfully.');
+    }
+    private function validateInput(StoreDoctorRequest|UpdateDoctorRequest $request)
+    {
+        $validatedData = $request->validate([
+            'name' => '',
+            'specialty' => '',
+            'clinic_id' => 'exclude_if:clinic_id,null|exists:clinics,id',
+            'clinic_name' => 'exclude_unless:clinic_id,null|string',
+            'clinic_address' => 'exclude_unless:clinic_id,null|string',
+        ]);
+
+        // If clinic_id has been excluded in the validation, process a new clinic:
+        if (!isset($validatedData['clinic_id'])) {
+            $newClinicAttr = [
+                'name' => $validatedData['clinic_name'],
+                'address' => $validatedData['clinic_address'],
+            ];
+            unset($validatedData['clinic_name']);
+            unset($validatedData['clinic_address']);
+
+            $newClinic = Clinic::create($newClinicAttr);
+
+            $validatedData['clinic_id'] = $newClinic->id;
+        }
+
+        return $validatedData;
     }
 }
--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -30,13 +30,29 @@
         <div class="mb-4">
             <label for="clinic_id" class="block mb-2">Clinic</label>
             <select name="clinic_id" id="clinic_id" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                <option value="">Select Clinic</option>
+                <option value="">Add new (provide data below) or select</option>
                 @foreach($clinics as $clinic)
-                    <option value="{{ $clinic->id }}" {{ old('clinic_id') == $clinic->id ? 'selected' : '' }}>{{ $clinic->name }} ({{ $clinic->address }})</option>
+                    <option value="{{ $clinic->id }}" {{ $doctor->clinic_id == $clinic->id ? 'selected' : 'data-dump['. old('clinic_id'). ']' }}>{{ $clinic->name }} ({{ $clinic->address }})</option>
                 @endforeach
             </select>
             @error('clinic_id')
             <p class="text-red-500 mt-1">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-4">
+            <label for="clinic_name" class="block mb-2">Clinic Name</label>
+            <input type="text" name="clinic_name" id="clinic_name" value="{{ old('clinic_name') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            @error('clinic_name')
+                <p class="text-red-500 mt-1">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="mb-4">
+            <label for="clinic_address" class="block mb-2">Clinic Address</label>
+            <input type="text" name="clinic_address" id="clinic_address" value="{{ old('clinic_address') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            @error('clinic_address')
+                <p class="text-red-500 mt-1">{{ $message }}</p>
             @enderror
         </div>
 
